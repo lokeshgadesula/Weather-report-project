e7ab1dabd219c8ec4fa2b558ffc4ff2864598111
Lokeshprasanth <84801492+lokeshgadesula@users.noreply.github.com>
2021-09-13 15:25:28 +0530

    Update AI weather report.py

--- AI weather report.py
+++ AI weather report.py
@@ -1,93 +1,25 @@
-import JarvisAI
-import re
-import pprint
-import random
+import requests
 
-obj = JarvisAI.JarvisAssistant()
+from datetime import datetime
 
+api_key = '7d0bee86bd92f39b064df3f008a5f20e'
+location = input("enter the city name: ")
 
-def t2s(text):
-    obj.text2speech(text)
+complete_api_link = "http://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=7d0bee86bd92f39b064df3f008a5f20e"
+api_link = requests.get(complete_api_link)
+api_data = api_link.json()
 
+temp_city = ((api_data['main']['temp']) - 273.15)
+weather_desc = api_data['weather'][0]['description']
+hmdt = api_data['main']['humidity']
+wind_spd = api_data['wind']['speed']
+date_time = datetime.now().strftime("%d %b %Y | %I:%M:%S %p")
 
-while True:
-    res = obj.mic_input()
+print("-----------------------------------------------------------")
+print("Weather Stats for - {} || {}".format(location.upper(), date_time))
+print("-----------------------------------------------------------")
 
-    if re.search('weather|temperature', res):
-        city = res.split(' ')[-1]
-        weather_res = obj.weather(city=city)
-        print(weather_res)
-        t2s(weather_res)
-
-    if re.search('news', res):
-        news_res = obj.news()
-        pprint.pprint(news_res)
-        t2s(f"I have found {len(news_res)} news. You can read it. Let me tell you first 2 of them")
-        t2s(news_res[0])
-        t2s(news_res[1])
-
-    if re.search('tell me about', res):
-        topic = res.split(' ')[-1]
-        wiki_res = obj.tell_me(topic)
-        print(wiki_res)
-        t2s(wiki_res)
-
-    if re.search('date', res):
-        date = obj.tell_me_date()
-        print(date)
-        print(t2s(date))
-
-    if re.search('time', res):
-        time = obj.tell_me_time()
-        print(time)
-        t2s(time)
-
-    if re.search('open', res):
-        domain = res.split(' ')[-1]
-        open_result = obj.website_opener(domain)
-        print(open_result)
-
-    if re.search('launch', res):
-        dict_app = {
-            'chrome': 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe',
-            'epic games': 'C:\Program Files (x86)\Epic Games\Launcher\Portal\Binaries\Win32\EpicGamesLauncher.exe'
-        }
-
-        app = res.split(' ', 1)[1]
-        path = dict_app.get(app)
-        if path is None:
-            t2s('Application path not found')
-            print('Application path not found')
-        else:
-            t2s('Launching: ' + app)
-            obj.launch_any_app(path_of_app=path)
-
-    if re.search('hello', res):
-        print('Hi')
-        t2s('Hi')
-
-    if re.search('how are you', res):
-        li = ['good', 'fine', 'great']
-        response = random.choice(li)
-        print(f"I am {response}")
-        t2s(f"I am {response}")
-
-    if re.search('your name|who are you', res):
-        print("My name is Jarvis, I am your personal assistant")
-        t2s("My name is Jarvis, I am your personal assistant")
-
-    if re.search('what can you do', res):
-        li_commands = {
-            "open websites": "Example: 'open youtube.com",
-            "time": "Example: 'what time it is?'",
-            "date": "Example: 'what date it is?'",
-            "launch applications": "Example: 'launch chrome'",
-            "tell me": "Example: 'tell me about India'",
-            "weather": "Example: 'what weather/temperature in Mumbai?'",
-            "news": "Example: 'news for today' ",
-        }
-        ans = """I can do lots of things, for example you can ask me time, date, weather in your city,
-        I can open websites for you, launch application and more. See the list of commands-"""
-        print(ans)
-        pprint.pprint(li_commands)
-        t2s(ans)
+print("current tempareture is: {:.2f} deg C".format(temp_city))
+print("current weather desc :", weather_desc)
+print("current Humidity     :", hmdt, '%')
+print("current wind speed :", wind_spd, 'kmph')
